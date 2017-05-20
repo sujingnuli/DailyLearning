dd8f4e1a0d9ac971e1244f9e3093704e0403e868
Jane <1558073257@qq.com>
2017-05-21 01:29:09 +0800

    c#

---
+++ csharp/ASP.NETMVCWeb/code/EBuy/Ebuy.Core/Clz/ProductsRepository.cs
@@ -0,0 +1,33 @@
+﻿using Ebuy.Core.Inter;
+using EBuy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebuy.Core.Clz
+{
+    public class ProductsRepository<T>:ISearchProvider,IRepository<T>
+    {
+        public T GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Search<T>(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
