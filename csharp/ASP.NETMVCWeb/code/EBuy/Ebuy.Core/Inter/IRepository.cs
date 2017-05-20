05ee39c3bd63ebbcce01ade4e2a129db4bab91b6
Jane <1558073257@qq.com>
2017-05-21 01:30:02 +0800

    c#

---
+++ csharp/ASP.NETMVCWeb/code/EBuy/Ebuy.Core/Inter/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebuy.Core.Inter
+{
+    interface IRepository<T>
+    {
+        T GetById(string id);
+        void Delete(T t);
+        void Save(T t);
+    }
+}
---
+++ csharp/ASP.NETMVCWeb/code/EBuy/Ebuy.Core/Inter/ISearchProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebuy.Core.Inter
+{
+    public interface ISearchProvider
+    {
+        List<T> Search<T>(string criteria);
+    }
+}
