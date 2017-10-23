21cc6d5700e48d0a8a3bcaa4fa4b6835e6534186
Jane <1558073257@qq.com>
2017-10-23 16:21:28 +0800

    t

---
+++ GMS/content/1.Login页面.cs
@@ -0,0 +1,5 @@
+1.Login页面
+	<input type="text" name="username" placeholder="Username"/>
+	<input type="password" name="password" placeholder="Password"/>
+	<input type="text" name="verifycode" placeholder="验证码"/>
+	<input type="submit" class="btn green m-icon-white"/>
